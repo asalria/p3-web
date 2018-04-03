3ed613cb36fd59cc0496e6865e9e6cefb142e848
Amelia Salgado <amelie@MacBook-Pro-de-Amelia.local>
2018-04-03 20:59:31 +0200

    feat: Nav-bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/misc/nav-bar/nav-bar.component';
+import { LoginComponent } from './components/misc/login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
